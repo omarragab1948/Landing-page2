3658452ef5316cd58a37a6673746e9c6747330e9
omarragab1948 <70483549+omarragab1948@users.noreply.github.com>
2022-09-24 05:14:20 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let ulNav = document.getElementById("navbar__list");
+let url="#section"
+for(let i = 0; i < (document.querySelectorAll("section").length); i++){
+    document.write(`<a href="#section${i+1}">Section ${i+1}</a>`)
+}
+for(let i = 0; i < document.links.length; i++){
+    document.querySelectorAll("a")[i].className = `section${i+1}`
+}
+for(let i = 0; i < document.links.length; i++){
+    ulNav.appendChild(document.querySelectorAll("a")[i])
+}
+
+window.onscroll = function (){
+    if (window.scrollY <= 620 && window.scrollY >= 615){
+       for(i = 1; i < document.links.length; i++){
+        document.getElementById(`section${i}`).classList.remove("your-active-class")
+       }
+       document.getElementById("section1").classList.toggle("your-active-class");
+    }  else if ((window.scrollY <= 1320 && window.scrollY >= 1250)){
+        for(i = 1; i < document.links.length; i++){
+            document.getElementById(`section${i}`).classList.remove("your-active-class")
+           }
+           document.getElementById("section2").classList.toggle("your-active-class");
+    }else if ((window.scrollY <= 1900 && window.scrollY >= 1890)){
+        for(i = 1; i < document.links.length; i++){
+            document.getElementById(`section${i}`).classList.remove("your-active-class")
+           }
+           document.getElementById("section3").classList.toggle("your-active-class");
+    }else if((window.scrollY <= 2500 && window.scrollY >= 2490)){
+        for(i = 1; i < document.links.length; i++){
+            document.getElementById(`section${i}`).classList.remove("your-active-class")
+           }
+           document.getElementById("section4").classList.toggle("your-active-class");
+    } else if((window.scrollY <= 3100 && window.scrollY >= 3090)){
+        for(i = 1; i < document.links.length; i++){
+            document.getElementById(`section${i}`).classList.remove("your-active-class")
+           }
+           document.getElementById("section5").classList.toggle("your-active-class");
+    }
+}
+
+
